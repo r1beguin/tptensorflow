8ef79c1f9c827ca469abc35cc3b2af8815f2391d
Erwan Béguin <erwan.beguin@gmail.com>
2020-11-19 14:50:42 +0100

    setup

---
+++ src/features/RangePredictor/RangePredictor.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import {Box, Text} from "grommet";
+
+
+const RangePredictor = () => {
+
+
+
+    return(
+        <Box>
+            <Text>RangePredictor</Text>
+        </Box>
+    )
+}
+
+
+export default RangePredictor;
